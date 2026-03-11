4eca3d820cf853af64f3f8978eb979e8d3a36eb0
vbalinm <v.balint0817@gmail.com>
2026-03-11 14:14:11 +0100

    Remove duplicate FortunaCasino folder

--- FortunaCasino/DTOs/Auth/AuthResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FortunaCasino.DTOs.Auth;
-
-public class AuthResponse
-{
-    public string Token { get; set; } = string.Empty;
-    public UserDto User { get; set; } = null!;
-}
--- FortunaCasino/DTOs/UserDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FortunaCasino.DTOs;
-
-public class UserDto
-{
-    public long Id { get; set; }
-    public string Username { get; set; } = string.Empty;
-    public decimal Balance { get; set; }
-    public List<string> Roles { get; set; } = new();
-}
